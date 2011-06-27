da9ad5dd4f654ddff9c8eb369ab0561f30e78e4a
Simon Oberhammer <simon@nekapuzer.at>
2011-06-27 14:07:23 +0200

    undo change to levels.js, see banditracer-server

--- javascript/levels.js
+++ javascript/levels.js
@@ -1,1 +1,1 @@
-exports.levels={'level1':require('./levels/level1').data,'level2':require('./levels/level2').data,'level3':require('./levels/level3').data};
+exports.levels={'level1':require('./levels/level1'),'level2':require('./levels/level2'),'level3':require('./levels/level3')};
